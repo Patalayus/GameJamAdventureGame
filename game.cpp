85d6df4f13863b0803810a20501c610da3a4f5a2
Patalayus <lukecharles222@gmail.com>
2020-01-31 21:57:43 +0000

    introduction

--- game.cpp
+++ game.cpp
@@ -1,7 +1,11 @@
+//this is the base code for my Game Jam project 2020
 #include <iostream>
 using namespace std;
 
 int main(){
     int answer1, answer2;
     std::cout << "please enter your name" << std::endl;
+    cin >> answer1;
+    cout << "welcome to my game! " << answer1;
+    return 0;
 }
