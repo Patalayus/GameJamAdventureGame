0732ee1c5a012f9413472b7d6d4b57cefc422bbb
sandeepbeedh <48889719+sandeepbeedh@users.noreply.github.com>
2020-01-31 18:55:36 +0000

    Create BasicStart.cpp

---
+++ BasicStart.cpp
@@ -0,0 +1,16 @@
+// TheOnslaughtOfFerocia.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cstring>
+#include <string>	
+#include <random>
+#include <cstdlib>
+#include "hlc2.h"
+
+//include string support
+
+using namespace std;			//using standard namespace
