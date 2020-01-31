3fcde4cbe20ddf69a1cea4540695567efe6efe36
Patalayus <lukecharles222@gmail.com>
2020-01-31 22:26:31 +0000

    std:: implementation

--- game.cpp
+++ game.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int answer1, answer2;
     std::cout << "please enter your name" << std::endl;
-    cin >> answer1;
-    cout << "welcome to my game! " << answer1;
+    std::cin >> answer1;
+    std::cout << "welcome to my game! " << answer1;
     return 0;
 }
