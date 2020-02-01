b2a92005b4537dea05c4e9d6737de0ac62734855
Patalayus <lukecharles222@gmail.com>
2020-02-01 20:22:37 +0000

    choice routine

--- game.cpp
+++ game.cpp
@@ -5,12 +5,20 @@
 using namespace std;
 
 void introduction(){
+    int answer3;
     std::cout << "It was a long time ago and the night was young" << std::endl;
     std::cout << "the lone traveller was on his mount again riding though the darkness" << std::endl;
     std::cout << "when all of a sudden he dropped his blade.. the Tri-Glave. The blade to end all wars..." << std::endl;
     std::cout << "the blade smashed into 5 different pieces and its magical power destroyed" << std::endl;
     std::cout << "many still say the blade is recovervable and that it can be mended, but others believe its magic to be lost forever" << std::endl;
-    std::cout << "You must be the one to find all 5 pieces of the blade to redeem its power and help to end all future wars." << std::endl;
+    std::cout << "You must be the one to find all 5 pieces of the blade to redeem its power and help to end all future wars.\nDo you accept this quest?\n1. Yes\n2. No" << std::endl;
+    std::cin >> answer3;
+    if(answer3==1){
+        std::cout << "alright lets get to it!" << std::endl;
+    }else if(answer3==2){
+        std::cout << "ah okay then..." << std::endl;
+        introduction();
+    }
 }
 
 int main(){
@@ -31,3 +39,6 @@ class characterIdentity{
         string name;
 };
 
+void secondary(){
+
+}
